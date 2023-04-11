f722bde2637c9753933e1404cfe92113e9aa2ef0
John Deck <jdeck88@gmail.com>
2023-04-10 17:53:54 -0700

    Update vendor_by_month.sql

--- scripts/vendor_by_month.sql
+++ scripts/vendor_by_month.sql
@@ -1,4 +1,4 @@
-# report on vendor orders by month
+# report on vendor orders by month.  This script defaults to the previous month
 select  monthname(o.time), s.vendor,sum(s.quantity*v.vendor_price) 
 from shop_orderitem s 
 	left join shop_productvariation v on v.sku = s.sku  
