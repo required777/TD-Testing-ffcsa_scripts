5becc0a2409520c757677a7e9db4f03bc6a98ef4
jdeck88 <jdeck88@gmail.com>
2024-03-04 16:37:29 +0000

    updating utilities script to correctly parset passed in dates

--- localline/utilities.js
+++ localline/utilities.js
@@ -318,9 +318,22 @@ function getPreviousWeek(dateString) {
 }
 
 function formatDate(date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-    const day = String(date.getDate()).padStart(2, '0');
+    const months = {
+        Jan: 0, Feb: 1, Mar: 2, Apr: 3, May: 4, Jun: 5,
+        Jul: 6, Aug: 7, Sep: 8, Oct: 9, Nov: 10, Dec: 11
+    };
+
+    // Split the string by space
+    const parts = date.split(' ');
+
+    // Extract day, month, and year
+    const day = parseInt(parts[0], 10);
+    const month = months[parts[1]];
+    const year = parseInt(parts[2], 10);
+
+    //const year = date.getFullYear();
+    //const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    //const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
 }
 function formatDateToYYYYMMDD(date) {
