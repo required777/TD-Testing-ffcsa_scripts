514a2612f68da5cbc0d55a6d0015d8002a6f4af9
jdeck88 <jdeck88@gmail.com>
2024-03-11 16:32:39 +0000

    adding note to weekly kpi report to direct to github location

--- localline/weekly_kpi.js
+++ localline/weekly_kpi.js
@@ -155,7 +155,7 @@ async function run(start, end) {
 				to: "fullfarmcsa@deckfamilyfarm.com",
 				cc: "jdeck88@gmail.com",
 				subject: subjectString,
-				text: JSON.stringify(salesData, null, 4)
+				text: JSON.stringify(salesData, null, 4) + "\n\nRunning KPI stats viewable at:\nhttps://github.com/jdeck88/ffcsa_scripts/blob/main/localline/data/weekly_kpi.csv"
 			};
 			utilities.sendEmail(emailOptions)
   		}, 3000);
