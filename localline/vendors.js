cf1a9ae05f69baf84521e4b77e6e1049ba291d50
jdeck88 <jdeck88@gmail.com>
2024-06-17 20:42:54 +0000

    remove TEST wording in message

--- localline/vendors.js
+++ localline/vendors.js
@@ -196,7 +196,7 @@ async function writeVendorsPDF(products_file_path, vendors_file_path, filename) 
                                                 to: email,                                            
                                                 cc: 'fullfarmcsa@deckfamilyfarm.com',
                                                 bcc: 'jdeck88@gmail.com',
-                                                subject: "TEST Full Farm CSA - " + vendorName + " - " + utilities.getToday(), // Subject line
+                                                subject: "Full Farm CSA - " + vendorName + " - " + utilities.getToday(), // Subject line
                                                 text: "The attached PDF file contains the Full Farm CSA Order for the next fulfillment Cycle.  " +
                                                     "Respond to this email (including both cc:ed addresses) with questions!"
                                             }
