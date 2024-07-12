2ae942d31a9ccf756187f48d54240465b7e385e7
jdeck88 <jdeck88@gmail.com>
2024-07-12 00:20:31 +0000

    updating monthly_customers script

--- localline/monthly_customers.js
+++ localline/monthly_customers.js
@@ -8,28 +8,58 @@ const PDFDocument = require('pdfkit-table');
 const axios = require('axios');
 const fastcsv = require('fast-csv');
 
+pdf_file_name = '';
+pdf_file = '';
 
-async function run(filename, lastDay) {
+
+async function run(filename, lastDay, yesterday) {
     return new Promise((resolve, reject) => {
-        const pdf_file = 'data/customers_' + lastDay + '.pdf';
+
+        // Create a new PDF document
+        pdf_file_name = 'customers_' + yesterday + '.pdf';
+        pdf_file = 'data/' + pdf_file_name
+
+        const doc = new PDFDocument();
+        doc.pipe(fs.createWriteStream(pdf_file));
+
         const customers = [];
 
         fs.createReadStream(filename)
             .pipe(fastcsv.parse({ headers: true }))
             .on('data', (row) => {
                 customers.push({
-                    amount: row['Store Credit']
+                    name: row['Customer'], // Assuming 'Customer Name' is a column in your CSV
+                    amount: parseFloat(row['Store Credit'])
                 });
             })
             .on('end', () => {
 
                 all_customer_sales = 0
+                let customerData = [];
+
+                customers.sort((a, b) => b.amount - a.amount);
+
                 customers.forEach((item) => {
                     const { amount } = item;
                     all_customer_sales = all_customer_sales + parseFloat(amount)
+                    customerData.push([item.name, item.amount.toFixed(2)])
                 });
 
                 formattedBalance = all_customer_sales.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
+				doc.text('Total member balances = ' + formattedBalance + ' as of ' + yesterday + ' 11:59pm')
+
+   				table = {
+                            title: 'Customer Balances',
+                            widths: [600], // Set the width to the page width
+                            headers: ['customer', 'balance'],
+                            rows: customerData,
+                };
+
+                doc.table(table);
+
+                doc.end();
+
 
                 // TODO: figure out appropriate aync methods to enable finishing PDF creation
                 setTimeout(() => {
@@ -44,7 +74,7 @@ async function run(filename, lastDay) {
 }
 
 // Build customer delivery orders (picklists)
-async function customers(today) {
+async function customers(today, yesterday) {
     try {
         console.log("running monthly customer balance report")
 
@@ -63,19 +93,25 @@ async function customers(today) {
 
                 // Download File
                 if (url !== "") {
-                    utilities.downloadBinaryData(url, 'data/customers_' + today + ".csv", accessToken)
+                    utilities.downloadBinaryData(url, 'data/customers_' + today + '.csv', accessToken)
                         .then((customer_file_path) => {
                             console.log('Downloaded file path:', customer_file_path);
-                            run(customer_file_path, today).then((balance) => {
+                            run(customer_file_path, today, yesterday).then((balance) => {
                                 try {
                                     const emailOptions = {
                                         from: "jdeck88@gmail.com",
                                         //to: "fullfarmcsa@deckfamilyfarm.com",
+										//cc: Margaret Hobart <mhobart@bworcpas.com> after verifying report data
                                         to: "jdeck88@gmail.com",
-                                        subject: 'FFCSA Reports: Monthly Customer Balance Report for ' + today,
-                                        text: "Total Member Balance as of " + today + " = " + balance,
+                                        subject: 'FFCSA Reports: Monthly Customer Balance Report for ' + yesterday,
+                                        text: "Total Member Balance as of " + yesterday+ " = " + balance,
                                     };
                                    
+                            		emailOptions.attachments = [
+                                	{
+                                    	filename: pdf_file_name, // Change the filename as needed
+                                    	content: fs.readFileSync(pdf_file) // Attach the file buffer
+                            		}];
                                     utilities.sendEmail(emailOptions)
 
                                 } catch (error) {
@@ -107,7 +143,4 @@ async function customers(today) {
 }
 
 //today = '2023-10-31'
-customers(utilities.getToday());
-
-
-
+customers(utilities.getToday(), utilities.getYesterday());
--- localline/utilities.js
+++ localline/utilities.js
@@ -428,6 +428,18 @@ function getOrderDayMinusFourteen() {
     return yesterdayFormatted;
 }
 
+function getYesterday() {
+    const today = new Date();
+    today.setDate(today.getDate() - 1); // Subtract one day
+
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const day = String(today.getDate()).padStart(2, '0');
+
+    const yesterdayFormatted = `${year}-${month}-${day}`;
+    return yesterdayFormatted;
+}
+
 function getToday() {
     const today = new Date();
 
@@ -497,5 +509,6 @@ module.exports = {
     getLastMonth,
     getToday,
     getTomorrow,
+    getYesterday,
     mailADocument
 };
