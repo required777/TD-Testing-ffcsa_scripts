122e099a594f69e308ed5e2707acf7615af8d88d
jdeck88 <jdeck88@gmail.com>
2024-07-16 00:41:12 +0000

    updating weekly_kpi.js to resume emails

--- localline/weekly_kpi.js
+++ localline/weekly_kpi.js
@@ -230,7 +230,7 @@ async function run(start, end) {
 				subject: subjectString,
 				text: JSON.stringify(salesData, null, 4) + "\n\nRunning KPI stats viewable at:\nhttps://github.com/jdeck88/ffcsa_scripts/blob/main/localline/data/weekly_kpi.csv"
 			};
-			//utilities.sendEmail(emailOptions)
+			utilities.sendEmail(emailOptions)
   		}, 3000);
 
 
